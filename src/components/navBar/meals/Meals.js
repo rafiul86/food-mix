57a4b05efda9fd380c949d1acc01c0088ce3a583
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-30 16:08:56 +0600

    search option added

---
+++ src/components/navBar/meals/Meals.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button ,Card} from 'react-bootstrap';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+const Meals = ({meal}) => {
+    const {strMeal,strMealThumb} = meal
+    console.log(meal)
+    return (
+        <div>
+ <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={strMealThumb} />
+  <Card.Body>
+    <Card.Title>{strMeal}</Card.Title>
+    <FontAwesomeIcon style={{color : 'gold'}} icon={faStar} />
+    <FontAwesomeIcon style={{color : 'gold'}} icon={faStar} />
+    <FontAwesomeIcon style={{color : 'gold'}} icon={faStar} />
+    <FontAwesomeIcon style={{color : 'gold'}} icon={faStar} />
+    <FontAwesomeIcon style={{color : 'gold'}} icon={faStar} />
+  </Card.Body>
+</Card>
+        </div>
+    );
+};
+
+export default Meals;
